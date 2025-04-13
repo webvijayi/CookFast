52f1e090958c8a65eb7dedd09a526c2c0c8bdc77
webvijayi <webvijayi.com@gmail.com>
2025-04-13 22:14:03 +0000

    feat: Update favicon link to use favicon.png

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Add link to the desired favicon */}
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
